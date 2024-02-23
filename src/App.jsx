003202506fbd7e794f2e3300c8b0a28c297bdcb5
claudiahW <murungiclaud@gmail.com>
2024-02-24 00:17:07 +0300

    create a new file for random component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 
+
 function App() {
 
   return (
   <div className="App">
     <h1>Hello World!</h1>
+    <RandomImage />
   </div>
-   
+  
   )
 }
 
---
+++ src/RandomImage.jsx
@@ -0,0 +1,3 @@
+function RandomImage() {
+    return <img src="https://picsum.photos/200/300" alt="random"/>
+  }
